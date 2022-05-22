f1f573084977c9cbb167af0044bbefd02c1af83b
Ivan <45919772+ivanMuzyka144@users.noreply.github.com>
2022-05-22 18:55:06 +0200

    Create basic elevator logic and add fpc

---
+++ Assets/CodeBase/Logic/Tower/Door.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+  [SerializeField] private Transform _leftDoor;
+  [SerializeField] private Transform _rightDoor;
+  [Space(10)]
+  [SerializeField] private Transform _leftDoorStartPoint;
+  [SerializeField] private Transform _leftDoorFinishPoint;
+  [SerializeField] private Transform _rightDoorStartPoint;
+  [SerializeField] private Transform _rightDoorFinishPoint;
+  [Space(10)] 
+  [SerializeField] private float _openCloseTime = 1;
+
+  public void OpenDoor()
+  {
+      _leftDoor.DOMove(_leftDoorFinishPoint.position, _openCloseTime);
+      _rightDoor.DOMove(_rightDoorFinishPoint.position, _openCloseTime);
+  }
+  
+  public void CloseDoor()
+  {
+      _leftDoor.DOMove(_leftDoorStartPoint.position, _openCloseTime);
+      _rightDoor.DOMove(_rightDoorStartPoint.position, _openCloseTime);
+  }
+  
+  
+}
---
+++ Assets/CodeBase/Logic/Tower/OpenCloseElevatorTrigger.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace CodeBase.Logic.Tower
+{
+    public class OpenCloseElevatorTrigger : MonoBehaviour
+    {
+        [SerializeField] private Door _door;
+        [SerializeField] private bool _shouldOpen;
+
+        private bool _isActivated;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player") && !_isActivated)
+            {
+                _isActivated = true;
+                
+                if (_shouldOpen)
+                    _door.OpenDoor();
+                else
+                    _door.CloseDoor();
+
+            }
+        }
+    }
+}
